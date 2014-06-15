e0104dcb4637fd3ff6f11fab07c8523ca40ad9d6
liu916 <zliu916@gmail.com>
2014-06-14 20:40:25 -0400

    add square class

--- main.cpp
+++ main.cpp
@@ -3,8 +3,40 @@
 
 #define TETRIS_BOX_SIZE 30
 
+class Square
+{
+  sf::Color color;
+  bool on;
+public:
+  void setColor(int r, int g, int b)
+  {
+    color.r = r;
+    color.g = g;
+    color.b = b;
+  }
+  void setOn()
+  {
+    on = true;
+  }
+  void setOff()
+  {
+    on = false;
+  }
+};
+
 int main()
 {
+
+  Square field[22][10];
+  for (int i = 0; i < 22; i++)
+    {
+      for (int j = 0; j < 10; j++)
+	{
+	  field[i][j].setColor(0, 0, 0);
+	  field[i][j].setOff();
+	}
+    }
+
   sf::RenderWindow window(sf::VideoMode(10 * TETRIS_BOX_SIZE, 20 * TETRIS_BOX_SIZE), "Tetris");
   
   while (window.isOpen())
